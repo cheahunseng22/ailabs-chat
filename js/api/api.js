f8cc91b70a0121c9a0f9624547c7190223a157f3
Sreytha060319 <sreytha285@gmail.com>
2026-06-04 14:07:25 +0700

    last

--- js/api/api.js
+++ js/api/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://cheaper-details-how-nominated.trycloudflare.com";
+const API_BASE = "https://forums-lcd-writer-decor.trycloudflare.com";
 const API_KEY = "sk_7X3kL9mN2pQ5rT8vW1yZ4aB6cD0eF3gH5jK7lM9nP1qR3tV5wX7yZ";
 
 let currentController = null;
